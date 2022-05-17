29aa15dab7509f9675df37a72231f15d02f9f069
Sakiko Yoshida <sacco221@gmail.com>
2022-05-18 08:53:34 +0900

    create a base of blog and add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  get "/articles", to: "articles#index"
+  devise_for :users
+  root to: "articles#index"
+#  get "/articles", to: "articles#index"
+#  get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles do
+    resources :comments
+  end
  
 end
