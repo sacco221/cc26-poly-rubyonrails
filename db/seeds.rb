053812f6e01c42ada4d3c52dd9f96711bd45078c
Sakiko Yoshida <sacco221@gmail.com>
2022-05-18 23:11:03 +0900

    add CSS and some functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create(title: "This is my first article here" , body: "This week, I start learning RoR!" , status: "private")
+Article.create(title: "Where should I start?" , body: "let's check its tutorials!", status: "public")
+Article.create(title: "日本語での投稿にも対応しているよ" , body: "日本語でも記録を残すよ", status: "public")
+Article.create(title: "Day 4: MVC" , body: "model, controller, view", status: "public")
+Article.create(title: "Day 5: .erb extension" , body: ".erb is Embeded Ruby. I can embed ruby expression into html with this.", status: "public")
